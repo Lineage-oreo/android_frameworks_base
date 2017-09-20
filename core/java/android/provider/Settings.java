0a9e3ce9c498dab937f51b70f7683fb0698ec588
maxwen <max.weninger@gmail.com>
2017-09-20 20:54:41 +0800

    [1/2] base: unlink notification volume is back -delete old unused volume images -add new one for notification volumes maybe some day one will be able to create a vector drawable for this - I am not :)
    base: add vector for notification volume mute
    
    thanks to Marko Man

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3996,6 +3996,13 @@ public final class Settings {
         public static final String KEY_APP_SWITCH_LONG_PRESS_ACTION = "key_app_switch_long_press_action";
 
         /**
+         * Boolean value whether to link ringtone and notification volume
+         *
+         * @hide
+         */
+        public static final String VOLUME_LINK_NOTIFICATION = "volume_link_notification";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
