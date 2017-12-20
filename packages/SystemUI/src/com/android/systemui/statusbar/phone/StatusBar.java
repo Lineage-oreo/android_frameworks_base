c68d5dcd05c27d16972b8ba9f20433674491e886
maxwen <max.weninger@gmail.com>
2017-12-20 14:38:41 +0000

    [1/2] base: configure SystemUI theme
    allow to force light or dark or use the wallpaper based
    automatic selection
    
    [Kshitij Gupta]
    - Adapt for extra DarkUI overlays
    - Refactor some variables. We're theming System, not just
      SystemUI
    
    Change-Id: Ie74ee40c7c054b259a6fb907ab83060769b6d8dd
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -843,6 +843,18 @@ public class StatusBar extends SystemUI implements DemoMode,
            mContext.getContentResolver().registerContentObserver(Settings.System.getUriFor(
                    Settings.System.ENABLE_HW_KEYS),
                    false, this, UserHandle.USER_ALL);
+	   mContext.getContentResolver().registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.SYSTEM_THEME_STYLE),
+                    false, this, UserHandle.USER_ALL);
+        }
+
+	@Override
+        public void onChange(boolean selfChange, Uri uri) {
+            if (uri.equals(Settings.System.getUriFor(Settings.System.SYSTEM_THEME_STYLE))) {
+                updateTheme();
+                return;
+            }
+            update();
         }
 
         @Override
@@ -4916,11 +4928,18 @@ public class StatusBar extends SystemUI implements DemoMode,
     protected void updateTheme() {
         final boolean inflated = mStackScroller != null;
 
-        // The system wallpaper defines if QS should be light or dark.
-        WallpaperColors systemColors = mColorExtractor
-                .getWallpaperColors(WallpaperManager.FLAG_SYSTEM);
-        final boolean useDarkTheme = systemColors != null
-                && (systemColors.getColorHints() & WallpaperColors.HINT_SUPPORTS_DARK_THEME) != 0;
+	int userThemeSetting = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.SYSTEM_THEME_STYLE, 0, mCurrentUserId);
+        boolean useDarkTheme = false;
+        if (userThemeSetting == 0) {
+            // The system wallpaper defines if QS should be light or dark.
+            WallpaperColors systemColors = mColorExtractor
+                    .getWallpaperColors(WallpaperManager.FLAG_SYSTEM);
+            useDarkTheme = systemColors != null
+                    && (systemColors.getColorHints() & WallpaperColors.HINT_SUPPORTS_DARK_THEME) != 0;
+        } else {
+            useDarkTheme = userThemeSetting == 2;
+        }
         if (isUsingDarkTheme() != useDarkTheme) {
             try {
                 mOverlayManager.setEnabled("com.android.system.theme.dark",
